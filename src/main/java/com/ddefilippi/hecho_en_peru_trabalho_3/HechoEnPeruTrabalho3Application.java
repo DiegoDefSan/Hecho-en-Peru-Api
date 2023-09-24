b33a89609f5b656ad494500a4ef2ee8390c7ef80
DiegoDefSan <defsan35@gmail.com>
2023-09-24 18:13:12 -0300

    first commit

---
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/HechoEnPeruTrabalho3Application.java
@@ -0,0 +1,13 @@
+package com.ddefilippi.hecho_en_peru_trabalho_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HechoEnPeruTrabalho3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HechoEnPeruTrabalho3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddefilippi/hecho_en_peru_trabalho_3/HechoEnPeruTrabalho3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddefilippi.hecho_en_peru_trabalho_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HechoEnPeruTrabalho3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
