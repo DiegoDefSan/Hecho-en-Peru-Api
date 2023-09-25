3dbaaa996ed96f8b795627ef2f5b0289ec04d658
DiegoDefSan <defsan35@gmail.com>
2023-09-25 18:25:36 -0300

    feat: category model relation one to many added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Category.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Category.java
@@ -1,12 +1,12 @@
 package com.ddefilippi.hecho_en_peru_trabalho_3.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,4 +19,7 @@ public class Category {
     @GeneratedValue(generator = "category_generator")
     private String idCategory;
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products = new ArrayList<>();
 }
