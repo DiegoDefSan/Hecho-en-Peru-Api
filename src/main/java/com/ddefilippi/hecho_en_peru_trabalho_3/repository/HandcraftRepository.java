b1e462fa57449ccf078cfc0dc907aae2e9645eb2
DiegoDefSan <defsan35@gmail.com>
2023-09-25 18:56:00 -0300

    feat: handcraft repository added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/repository/HandcraftRepository.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/repository/HandcraftRepository.java
@@ -1,2 +1,7 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.repository;public interface HandcraftRepository {
+package com.ddefilippi.hecho_en_peru_trabalho_3.repository;
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.Handcraft;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HandcraftRepository extends JpaRepository<Handcraft, String> {
 }
