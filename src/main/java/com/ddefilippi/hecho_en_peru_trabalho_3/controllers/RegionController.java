9b239b990d4a44b0a01806a0f303bcd9ff95cc2b
DiegoDefSan <defsan35@gmail.com>
2023-09-25 20:00:45 -0300

    feat: region controller added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/controllers/RegionController.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/controllers/RegionController.java
@@ -1,2 +1,29 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.controllers;public class RegionController {
+package com.ddefilippi.hecho_en_peru_trabalho_3.controllers;
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.Region;
+import com.ddefilippi.hecho_en_peru_trabalho_3.service.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+@RequestMapping("/regions") // http://localhost:8080/regions
+public class RegionController {
+
+    @Autowired
+    private RegionService regionService;
+
+    @GetMapping
+    public List<Region> getRegions() {
+        return regionService.getRegions();
+    }
+
+    @PostMapping
+    public List<Region> saveRegions(List<Region> regions) {
+        return regionService.saveRegions(regions);
+    }
 }
