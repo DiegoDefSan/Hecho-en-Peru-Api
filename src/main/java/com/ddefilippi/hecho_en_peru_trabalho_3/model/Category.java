9af3c029da7079d47b52a7db2a68ce130ad7bae1
DiegoDefSan <defsan35@gmail.com>
2023-09-25 18:34:26 -0300

    feat: category model modified

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Category.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Category.java
@@ -1,5 +1,6 @@
 package com.ddefilippi.hecho_en_peru_trabalho_3.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonIgnoreProperties(value = {"products"})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
