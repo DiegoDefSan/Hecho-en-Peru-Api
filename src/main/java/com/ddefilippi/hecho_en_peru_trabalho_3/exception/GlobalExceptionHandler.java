29441420007c2dd76f3c4793453ec484017c220c
DiegoDefSan <defsan35@gmail.com>
2023-09-26 20:33:13 -0300

    feat: global exception handler modified - @ControllerAdvide added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.ddefilippi.hecho_en_peru_trabalho_3.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         @ExceptionHandler(HighlightedEntityException.class)
