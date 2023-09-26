be0bef158f0a8f5681982321fbf363a53e8592c0
DiegoDefSan <defsan35@gmail.com>
2023-09-26 19:36:03 -0300

    feat: highlighted entity exception added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/exception/HighlightedEntityException.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/exception/HighlightedEntityException.java
@@ -1,2 +1,7 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.exception;public class HighlightedEntityException {
+package com.ddefilippi.hecho_en_peru_trabalho_3.exception;
+
+public class HighlightedEntityException extends RuntimeException {
+    public HighlightedEntityException(String message) {
+        super(message);
+    }
 }
