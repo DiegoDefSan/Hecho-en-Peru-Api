92247a2acb217e3a177f36eac66ec19eca2e93e1
DiegoDefSan <defsan35@gmail.com>
2023-09-26 19:36:23 -0300

    feat: transient entity exception added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/exception/TransientEntityException.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/exception/TransientEntityException.java
@@ -1,2 +1,7 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.exception;public class TransientEntityException {
+package com.ddefilippi.hecho_en_peru_trabalho_3.exception;
+
+public class TransientEntityException extends RuntimeException {
+    public TransientEntityException(String message) {
+        super(message);
+    }
 }
