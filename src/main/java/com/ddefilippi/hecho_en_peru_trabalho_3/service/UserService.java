2749248238825cb27601b709b972262b7ce27a9b
DiegoDefSan <defsan35@gmail.com>
2023-12-16 14:56:01 -0300

    feat: user service added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/UserService.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/UserService.java
@@ -1,2 +1,23 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.service;public class UserService {
+package com.ddefilippi.hecho_en_peru_trabalho_3.service;
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.User;
+import com.ddefilippi.hecho_en_peru_trabalho_3.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
