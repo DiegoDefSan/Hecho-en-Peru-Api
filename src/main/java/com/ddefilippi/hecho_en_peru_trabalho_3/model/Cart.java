2d01b4ca260aa0641aef0fbe2610a435dd528505
DiegoDefSan <defsan35@gmail.com>
2023-12-16 14:56:34 -0300

    feat: cart entity added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Cart.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Cart.java
@@ -1,2 +1,26 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.model;public class Cart {
+package com.ddefilippi.hecho_en_peru_trabalho_3.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="carts")
+public class Cart {
+    @Id
+    @GeneratedValue(generator = "cart_generator")
+    @GenericGenerator(
+            name = "cart_generator",
+            parameters = @org.hibernate.annotations.Parameter(name = "prefix", value = "CART"),
+            strategy = "com.ddefilippi.hecho_en_peru_trabalho_3.util.IdGenerator"
+    )
+    private String idCart;
+    private LocalDateTime date;
 }
