ef6d82573014ab680f88646c974ded0cdb52b2d1
DiegoDefSan <defsan35@gmail.com>
2023-12-16 14:57:04 -0300

    feat: cart service added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/CartService.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/CartService.java
@@ -1,2 +1,23 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.service;public class CartService {
+package com.ddefilippi.hecho_en_peru_trabalho_3.service;
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.Cart;
+import com.ddefilippi.hecho_en_peru_trabalho_3.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    public List<Cart> getCarts() {
+        return cartRepository.findAll();
+    }
+
+    public Cart saveCart(Cart cart) {
+        return cartRepository.save(cart);
+    }
 }
