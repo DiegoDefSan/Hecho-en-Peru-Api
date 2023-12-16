9ea0bdc470a6ee5806d97562b7de92a4c8104507
DiegoDefSan <defsan35@gmail.com>
2023-12-16 18:24:49 -0300

    feat: One to many -> ProductCart

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Product.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/Product.java
@@ -1,11 +1,16 @@
 package com.ddefilippi.hecho_en_peru_trabalho_3.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(value = {"productCarts"})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -40,6 +45,9 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "idHandcraft")
     private Handcraft handcraft;
+
+    @OneToMany(mappedBy = "product")
+    private List<ProductCart> productCarts = new ArrayList<>();
 
     public Product(String name, double price, int stock, String imagePath, double rating, String history, String details, Category category, Region region, Handcraft handcraft) {
         this.name = name;
