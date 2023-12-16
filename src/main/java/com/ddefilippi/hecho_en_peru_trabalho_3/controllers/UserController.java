3a077d2e613dfc00ce919c6795115cb7548a900f
DiegoDefSan <defsan35@gmail.com>
2023-12-16 14:56:15 -0300

    feat: user controller added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/controllers/UserController.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/controllers/UserController.java
@@ -1,2 +1,27 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.controllers;public class UserController {
+package com.ddefilippi.hecho_en_peru_trabalho_3.controllers;
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.User;
+import com.ddefilippi.hecho_en_peru_trabalho_3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/users") // http://localhost:8080/categories
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
 }
