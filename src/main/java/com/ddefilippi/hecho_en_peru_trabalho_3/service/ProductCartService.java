da6d2c85d5bad2dd63ad4a0e7c57515b9a1bff99
DiegoDefSan <defsan35@gmail.com>
2023-12-16 17:04:44 -0300

    feat: product-cart service added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/ProductCartService.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/ProductCartService.java
@@ -1,2 +1,22 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.service;public class ProductCartService {
+package com.ddefilippi.hecho_en_peru_trabalho_3.service;
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.ProductCart;
+import com.ddefilippi.hecho_en_peru_trabalho_3.repository.ProductCartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductCartService {
+    @Autowired
+    private ProductCartRepository productCartRepository;
+
+    public List<ProductCart> getProductCarts() {
+        return productCartRepository.findAll();
+    }
+
+    public ProductCart saveProductCart(ProductCart productCart) {
+        return productCartRepository.save(productCart);
+    }
 }
