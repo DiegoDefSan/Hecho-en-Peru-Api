0632efacf9544eef6dd124b90fbd253bb04412bf
DiegoDefSan <defsan35@gmail.com>
2023-12-16 14:55:22 -0300

    feat: user entity added

---
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/model/User.java
@@ -0,0 +1,28 @@
+package com.ddefilippi.hecho_en_peru_trabalho_3.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(generator = "user_generator")
+    @GenericGenerator(
+            name = "user_generator",
+            parameters = @org.hibernate.annotations.Parameter(name = "prefix", value = "USER"),
+            strategy = "com.ddefilippi.hecho_en_peru_trabalho_3.util.IdGenerator"
+    )
+    private String idUser;
+    private String name;
+}
