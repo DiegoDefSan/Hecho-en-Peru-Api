e8888c0105a766c57441740d531d08f1139db164
DiegoDefSan <defsan35@gmail.com>
2023-12-16 17:04:52 -0300

    feat: product-cart controller added

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/controllers/ProductCartController.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/controllers/ProductCartController.java
@@ -1,2 +1,27 @@
-package com.ddefilippi.hecho_en_peru_trabalho_3.controllers;public class ProductCartController {
+package com.ddefilippi.hecho_en_peru_trabalho_3.controllers;
+
+
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.ProductCart;
+import com.ddefilippi.hecho_en_peru_trabalho_3.service.ProductCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:5173")
+@RestController
+@RequestMapping("/product_cart") // http://localhost:8080/product_cart
+public class ProductCartController {
+    @Autowired
+    private ProductCartService productCartService;
+
+    @GetMapping
+    public List<ProductCart> getProductCarts() {
+        return productCartService.getProductCarts();
+    }
+
+    @PostMapping("/addProductCart")
+    public ProductCart addProductCart(@RequestBody ProductCart productCart) {
+        return productCartService.saveProductCart(productCart);
+    }
 }
