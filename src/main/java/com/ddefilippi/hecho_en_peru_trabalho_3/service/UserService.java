e5445b12128db8e659e7ac823754863e1a0a1012
DiegoDefSan <defsan35@gmail.com>
2023-12-17 08:17:27 -0300

    feat: get user by id

--- src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/UserService.java
+++ src/main/java/com/ddefilippi/hecho_en_peru_trabalho_3/service/UserService.java
@@ -1,5 +1,7 @@
 package com.ddefilippi.hecho_en_peru_trabalho_3.service;
 
+import com.ddefilippi.hecho_en_peru_trabalho_3.exception.EntityNotFoundException;
+import com.ddefilippi.hecho_en_peru_trabalho_3.model.Product;
 import com.ddefilippi.hecho_en_peru_trabalho_3.model.User;
 import com.ddefilippi.hecho_en_peru_trabalho_3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,13 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(
+                        () -> new EntityNotFoundException("User " + id + " not found")
+                );
     }
 
     public User saveUser(User user) {
